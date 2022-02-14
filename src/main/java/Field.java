c5bc3205594d8e94f010814df4395b0fcfe6d8bf
enzokid <overstill77@gmail.com>
2022-02-14 22:29:32 +0200

    java files added

---
+++ src/main/java/Field.java
@@ -0,0 +1,10 @@
+public class Field {
+
+        int x= 5;
+        public String returnValue (int x){
+            System.out.println("---<значение поля>---");
+            return String.valueOf(x);
+
+        }
+
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+        public static void main(String[] args) {
+            Field field= new Field();
+            String result= field.returnValue(field.x);
+            System.out.print("~~~");
+            System.out.print(result);
+            System.out.println("~~~");
+        }
+
+}
