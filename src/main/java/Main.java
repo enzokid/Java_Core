7ff1d649d7cce7f0ebbc5977e7bdb81c5adccbf8
enzokid <overstill77@gmail.com>
2022-02-15 14:47:49 +0200

    Add string format and .this

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,7 @@ public class Main {
         public static void main(String[] args) {
             Field field= new Field();
             String result= field.returnValue(field.x);
-            System.out.print("~~~");
-            System.out.print(result);
-            System.out.println("~~~");
+            System.out.println("~~~"+result+"~~~");
         }
 
 }
